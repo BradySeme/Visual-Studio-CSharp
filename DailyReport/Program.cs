6528e7cbed9d968e028172f99b7954a31ca9efd5
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-09-17 15:57:37 -0600

    Master

---
+++ DailyReport/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report\n");
+
+            //Name input here
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            //Course input here
+            Console.WriteLine("What course are you on?");
+            string coursenum = Console.ReadLine();
+
+            //Page input here
+            Console.WriteLine("What page number?");
+            string pagenum = Console.ReadLine();
+
+            //Help input here
+            Console.WriteLine("Do you need help with anything?");
+            Console.WriteLine("Please answer true or false.");
+            string help = Console.ReadLine();
+
+            //Positive input here
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string exp = Console.ReadLine();
+
+            //Feedback input here
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            //Hour input here
+            Console.WriteLine("How many hours did you study today?");
+            string hournum = Console.ReadLine();
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        }
+    }
+}

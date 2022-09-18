f7d7df3bbe0147096c96d1e89682d155ba146da9
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-09-18 16:24:21 -0600

    changes

---
+++ AssignmentDoAndDoWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AssignmentDoAndDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int n = 10;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.WriteLine();
+
+            do
+            {
+                Console.WriteLine(n);
+                n--;
+            }
+            while (n > 5);
+        }
+    }
+}

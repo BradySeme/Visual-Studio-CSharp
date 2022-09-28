857ff2283a768072f6ed428def869eb3901814b6
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-09-27 20:38:49 -0600

    changes

---
+++ MainMethodAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MathEquations mq = new MathEquations();
+
+        //v Put in any number here v
+            mq.Change("3");
+
+        }
+
+    }
+
+    class MathEquations
+    {
+        //First method adds
+        public void Change(int input)
+        {
+            int answer = input + 4;
+            Console.WriteLine($"{input} + 4 = {answer}");
+        }
+
+        //Second method subtracts
+        public void Change(double input)
+        {
+            double answer = input - 4;
+            Console.WriteLine($"{input} - 4 = {answer}");
+        }
+
+        //Third method multiplies
+        public void Change(string input)
+        {
+            double x = Double.Parse(input);
+            double answer = x * 4;
+            Console.WriteLine($"{input} * 4 = {answer}");
+        }
+
+    }
+}

b5ce5a7f2beb2215771258dee5b79ccc092a41c7
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-09-27 18:08:46 -0600

    changes

---
+++ ThreeMethodsAssignments/MathMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreeMethodsAssignments
+{
+    class MathMethods
+    {
+        public static void Plus(int input)
+        {
+            int answer = input + 4;
+            Console.WriteLine($"{input} + 4 = {answer}");
+        }
+
+        public static void Minus(int input)
+        {
+            int answer = input - 4;
+            Console.WriteLine($"{input} - 4 = {answer}");
+        }
+
+        public static void Multiply(int input)
+        {
+            int answer = input * 4;
+            Console.WriteLine($"{input} x 4 = {answer}");
+        }
+    }
+}
---
+++ ThreeMethodsAssignments/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ThreeMethodsAssignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: \n");
+            int answer = int.Parse(Console.ReadLine());
+
+            MathMethods.Plus(answer);
+            MathMethods.Minus(answer);
+            MathMethods.Multiply(answer);
+            
+        }
+    }
+}

08df88f9bf86bdd96c70de58533946cb995c5706
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-09-29 13:08:56 -0600

    changes

---
+++ MainMethodAssignment2/MathEquations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssignment2
+{
+    class MathEquations
+    {
+        public int Plus(int x, int y = 4)
+        {
+            int z = x + y;
+            return z;
+        }
+    }
+}
---
+++ MainMethodAssignment2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MainMethodAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring the class
+            MathEquations me = new MathEquations();
+            //Variables
+            int fnum;
+            int snum;
+            
+
+            //User input
+            Console.WriteLine("Enter in two numbers one at a time. You don't have to enter in a second number");
+            Console.Write("First number: ");
+            fnum = int.Parse(Console.ReadLine());
+            Console.Write("Second optional number: ");
+            
+            //If the user skips the second prompt, it will throw up an error, which will get caught below
+            try
+            {
+                snum = int.Parse(Console.ReadLine());
+                Console.WriteLine(me.Plus(fnum, snum));
+            }
+
+            //Here is where the exception will be caught 
+            catch (Exception)
+            {
+                Console.WriteLine(me.Plus(fnum));
+            }
+            
+        }
+    }
+
+
+}

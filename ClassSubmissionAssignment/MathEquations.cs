d8652e4f7f3905a98a69f9ca110943041a57373f
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-09-29 20:31:23 -0600

    changes

---
+++ ClassSubmissionAssignment/MathEquations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassSubmissionAssignment
+{
+    //This is the static class
+    public static class MathEquations
+    {
+        //The two methods below are being overloaded
+        public static void Half(double x)
+        {
+            Console.WriteLine(x / 2);
+        }
+
+        public static void Half(out string y)
+        {
+            y = "Have a nice day!";
+        }
+    }
+}
---
+++ ClassSubmissionAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring variable
+            string word;
+
+            //User input
+            Console.Write("Enter a number to divide by 2: ");
+            int fnum = int.Parse(Console.ReadLine());
+
+            //This method divides by two
+            MathEquations.Half(fnum);
+
+            //This method outputs to the variable "word"
+            MathEquations.Half(out word);
+
+            //This is what is being output
+            Console.WriteLine(word);
+        }
+    }
+}

a18bb0ff698406583f304739ece8c37a192ff0dd
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-09-29 19:23:25 -0600

    changes

---
+++ MainMethodAssigment3/MathEquations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssigment3
+{
+    class MathEquations
+    {
+        public void Plus(int x, int y)
+        {
+            int j = x + 4;
+            Console.WriteLine($"{x} + 4 = {j}");
+            Console.WriteLine($"{y} is your second number");
+        }
+    }
+}
---
+++ MainMethodAssigment3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MainMethodAssigment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiating the class
+            MathEquations me = new MathEquations();
+
+            //User input
+            Console.Write("Enter in your first number:");
+            int fnum = int.Parse(Console.ReadLine());
+            Console.Write("Enter in your second number:");
+            int snum = int.Parse(Console.ReadLine());
+
+            //"Call the method in the class, passing in two numbers"
+            me.Plus(fnum, snum);
+
+            //User input
+            Console.Write("Enter in your first number:");
+            fnum = int.Parse(Console.ReadLine());
+            Console.Write("Enter in your second number:");
+            snum = int.Parse(Console.ReadLine());
+
+            //"Call the method in the class, specifying the parameters by name"
+            me.Plus(x: fnum, y: snum);
+
+        }
+    }
+}

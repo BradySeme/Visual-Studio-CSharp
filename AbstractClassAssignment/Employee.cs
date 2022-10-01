7389d2cb46a1e29b784c58ed19afd1f0c3631424
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-10-01 17:21:13 -0600

    changes

---
+++ AbstractClassAssignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassAssignment
+{
+    //"Create another class called Employee and have it inherit from the Person class."
+    //"Implement the SayName() method inside of the Employee class."
+    public class Employee : Person
+    {
+        
+    }
+}
---
+++ AbstractClassAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //"Implement the SayName() method inside of the Employee class."
+            Employee em = new Employee();
+
+            //"Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”.
+            //Call the SayName() method on the object."
+
+            em.FirstName = "Sample ";
+            em.LastName = "Student";
+
+            em.SayName();            
+        }
+    }
+}

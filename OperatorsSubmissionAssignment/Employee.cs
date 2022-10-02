2d29c2251e56a449586469421652998760792c58
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-10-01 19:49:24 -0600

    changes

---
+++ OperatorsSubmissionAssignment/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsSubmissionAssignment
+{
+    public class Employee
+    {
+        //"Create an Employee class with Id, FirstName and LastName properties. "
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //"In the Employee class, overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property.
+        //Remember that comparison operators must be overloaded in pairs."
+        public static string operator== (Employee ID1, Employee ID2)
+        {
+            string answer;
+            if (ID1.ID == ID2.ID)
+            {
+                 answer = "The IDs match";
+            }
+            else
+            {
+                answer = "The IDs don't match";
+            }
+            return answer;
+        }
+
+        public static string operator !=(Employee ID1, Employee ID2)
+        {
+            string answer;
+            if (ID1.ID != ID2.ID)
+            {
+                answer = "The IDs don't match";
+            }
+            else
+            {
+                answer = "The IDs match";
+            }
+            return answer;
+        }
+    }
+}

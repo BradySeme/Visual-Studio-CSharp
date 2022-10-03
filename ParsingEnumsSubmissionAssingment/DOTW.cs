1319fe33de746ddf18d1b99abe5ad3e4ef37f239
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-10-02 20:52:18 -0600

    changes

---
+++ ParsingEnumsSubmissionAssingment/DOTW.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParsingEnumsSubmissionAssingment
+{
+    class DOTW
+    {
+        //"Create an enum for the days of the week."
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+        }
+    }
+}
---
+++ ParsingEnumsSubmissionAssingment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ParsingEnumsSubmissionAssingment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //"Prompt the user to enter the current day of the week."
+            Console.WriteLine("Enter in a day of the week.");
+            string day = Console.ReadLine();
+
+            try
+            {
+                //"Prompt the user to enter the current day of the week."
+                DOTW.DaysOfTheWeek day1 = (DOTW.DaysOfTheWeek)Enum.Parse(typeof(DOTW.DaysOfTheWeek), day);
+                Console.WriteLine(day1);
+            }
+            catch (Exception)
+            {
+                //"Wrap the above statement in a try/catch block and have it print "Please enter an actual day of the week.” to the console if an error occurs."
+                Console.WriteLine("Please enter an actual day of the week.");
+            }       
+
+        }
+    }
+}

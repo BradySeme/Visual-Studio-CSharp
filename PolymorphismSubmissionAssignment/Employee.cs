cf13a681295bc3055fe1b13fa9728f4993a9b364
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-10-03 18:44:53 -0600

    changes

--- PolymorphismSubmissionAssignment/Employee.cs
+++ PolymorphismSubmissionAssignment/Employee.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace PolymorphismSubmissionAssignment
 {
     //"Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose."
-    public class Employee : IQuittable
+    public class Employee : Person, IQuittable
     {
         public void Quit()
         {

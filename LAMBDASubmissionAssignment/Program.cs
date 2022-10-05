944a5c3ac4f3631430ae81055d00dea60bdb1c04
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-10-05 00:49:32 -0600

    changes

--- LAMBDASubmissionAssignment/Program.cs
+++ LAMBDASubmissionAssignment/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LAMBDASubmissionAssignment
 {
@@ -19,19 +20,34 @@ namespace LAMBDASubmissionAssignment
                 new Employee() { ID = 6, FirstName = "Joe", LastName = "Mama"},
                 new Employee() { ID = 7, FirstName = "Harry", LastName = "Anoos"},
                 new Employee() { ID = 8, FirstName = "Hugh", LastName = "Mungus"},
-                new Employee() { ID = 9, FirstName = "Anne", LastName = "Null"}
+                new Employee() { ID = 9, FirstName = "Chris", LastName = "Pratt"}
             };
 
-            var listofusers1 = listofusers                
 
+            List<Employee> listofusers2 = new List<Employee>();
             
 
-            foreach (string name in listofusers[em.FirstName])
+            foreach (Employee employee in listofusers)
             {
-                Console.WriteLine(name);
+                if (employee.FirstName == "Joe")
+                {
+                    listofusers2.Add(employee);
+                }
 
             }
 
+            List<Employee> listofusers3 = new List<Employee>();
+
+            listofusers3 = listofusers.Where(p => p.FirstName == "Joe").ToList();
+
+            foreach(Employee employee1 in listofusers3)
+            {
+                Console.WriteLine(employee1.FirstName);
+            }
+
+            List<Employee> listofusers4 = new List<Employee>();
+
+            listofusers4 = listofusers.Where(P => P.ID <= 5).ToList();
         }
     }
 }

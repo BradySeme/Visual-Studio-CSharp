83870202911148f680d8367dbac1264e2bbedb4d
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-10-07 21:27:27 -0600

    changes

---
+++ DateTimeAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = new DateTime();
+            time = DateTime.Now;
+
+            //"Prints the current date and time to the console."
+            Console.WriteLine(DateTime.Now);
+
+            //"Asks the user for a number."
+            Console.WriteLine("Enter a number");
+            int answer = int.Parse(Console.ReadLine());
+
+            //"Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2."
+            DateTime newtime = time.AddHours(answer);
+            Console.WriteLine(newtime);
+            
+        }
+    }
+}

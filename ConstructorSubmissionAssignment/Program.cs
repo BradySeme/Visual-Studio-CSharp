4d1af89adaf982ee21562722c10e4a923800f35e
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-10-08 19:54:17 -0600

    changes

---
+++ ConstructorSubmissionAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConstructorSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //"Create a const variable."
+            const string word = "Hello ";
+            //"Create a variable using the keyword 'var'."
+            var word2 = "World!";
+            Console.WriteLine(word + word2);
+
+            User sample = new User();
+
+            Console.WriteLine(sample.name + " " + sample.age);
+            
+            User sample2 = new User("Brady", 21);
+
+            Console.WriteLine(sample2.name + " " + sample2.age);
+
+        }
+    }
+}
---
+++ ConstructorSubmissionAssignment/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorSubmissionAssignment
+{
+    public class User
+    {
+        public string name;
+        public int age;
+
+        //"Chain two constructors together."
+        public User() : this("DefaultUser")
+        {
+        }
+
+        public User(string name) : this(name, 18)
+        { 
+        }
+
+        public User(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}

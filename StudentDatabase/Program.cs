4b9afa334a9f302d61172f50e4f3170b808ad4e1
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-10-20 00:29:08 -0600

    changes

---
+++ StudentDatabase/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StudentDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Brady Seme", Height = 6, Weight = 150 };
+                                          
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
